96d82f0617952b27930686ca86a93f1670b5dafd
darryl1975 <darryl1975@hotmail.com>
2023-12-05 10:41:43 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/d17lecture/D17LectureApplication.java
@@ -0,0 +1,17 @@
+package sg.edu.nus.iss.d17lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// References
+// https://github.com/public-api-lists/public-api-lists#currency-exchange
+// https://openlibrary.org/developers/api
+
+@SpringBootApplication
+public class D17LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D17LectureApplication.class, args);
+	}
+
+}
---
+++ src/main/java/sg/edu/nus/iss/d17lecture/controller/HomeController.java
@@ -0,0 +1,10 @@
+package sg.edu.nus.iss.d17lecture.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/home")
+public class HomeController {
+    
+}
